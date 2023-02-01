5677731e3adaca37d59951134cbdf8de362da7ff
y0nei <xyz.im@proton.me>
2023-02-01 18:11:33 +0100

    Add static directory

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 templates = Jinja2Templates(
         directory="app/templates",
