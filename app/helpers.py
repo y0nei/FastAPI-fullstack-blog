a6e7cebfb84f3899a521b559df96e9f4a913e45a
y0nei <xyz.im@proton.me>
2023-02-10 00:04:37 +0100

    Markdown syntax highlighting + Pygments
    This commit does the following:
    - Adds a custom html formatter using the codehilite markdown extension
      and Pygments
    - Adds `CodeHiliteExtension` to the `convertMarkdown` function

--- app/helpers.py
+++ app/helpers.py
@@ -1,6 +1,19 @@
 import markdown
 from fastapi import HTTPException
 
+from pygments.formatters import HtmlFormatter
+from markdown.extensions.codehilite import CodeHiliteExtension
+
+class CustomHtmlFormatter(HtmlFormatter):
+    def __init__(self, lang_str='', **options):
+        super().__init__(**options)
+        self.lang_str = lang_str
+
+    def _wrap_code(self, source):
+        yield 0, f'<code class="{self.lang_str}">'
+        yield from source
+        yield 0, '</code>'
+
 def parseMarkdown(content: str) -> tuple[dict[str, str], str]:
     md = markdown.Markdown(extensions=['meta'])
     md.convert(content)
@@ -22,7 +35,10 @@ def parseMarkdown(content: str) -> tuple[dict[str, str], str]:
     return md.Meta, body
 
 def convertMarkdown(content: str) -> str:
-    return markdown.markdown(content, extensions=['fenced_code'])
+    return markdown.markdown(content, extensions=[
+        'fenced_code',
+        CodeHiliteExtension(pygments_formatter=CustomHtmlFormatter, css_class="highlight")
+    ])
 
 def getMetadata(post_id: int):
     try:
