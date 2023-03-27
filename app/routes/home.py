4db0f84580fca916cb07016e752ab2b945b6a090
y0nei <xyz.im@proton.me>
2023-03-27 17:10:35 +0200

    feat(routes): hotreloading for home route

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter
 from fastapi import Request
+from app.hotreload import initHotreload
 from fastapi.responses import HTMLResponse
-
 from fastapi.templating import Jinja2Templates
+
+home_router = APIRouter(tags=["home"])
 templates = Jinja2Templates(
     directory="app/templates",
     lstrip_blocks=True, trim_blocks=True  # Whitespace control
 )
-
-home_router = APIRouter(tags=["home"])
+initHotreload(home_router, templates)
 
 @home_router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
