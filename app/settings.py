0f1cfbf5948eda94693a22f40e142cab27e5019b
y0nei <xyz.im@proton.me>
2023-03-29 16:02:40 +0200

    feat: Add mongodb + motor and database settings

--- app/settings.py
+++ app/settings.py
@@ -10,6 +10,15 @@ class Settings(BaseSettings):
     ENVIRONMENT: str = EnvType.DEVELOPMENT
     APP_HOST: str = "0.0.0.0"
     APP_PORT: int = 8000
+    SECRET_KEY: str
+
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int = 27017
+
+    MONGO_DATABASE: str
+    MONGO_COLLECTION: str
 
     class Config:
         env_file = ".env"
