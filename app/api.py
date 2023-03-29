00e30f07806a85daa952d5d4954d0554504d1039
y0nei <xyz.im@proton.me>
2023-03-29 17:40:35 +0200

    feat: SessionMiddleware from starlette

--- app/api.py
+++ app/api.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.settings import settings
 from app.routes import home, posts, article
+from starlette.middleware.sessions import SessionMiddleware
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
 
 if settings.DEBUG == 1:
     print(settings.dict())
