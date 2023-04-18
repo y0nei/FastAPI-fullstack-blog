0405ce52379614f29ddf2d54c43e369ea98ce9ab
y0nei <xyz.im@proton.me>
2023-04-18 23:52:52 +0200

    refactor(sessions): improve logic & add timestamps

--- app/api.py
+++ app/api.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from app.settings import settings
 from app.routes import home, posts, article
@@ -7,6 +8,17 @@ from starlette.middleware.sessions import SessionMiddleware
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
+
+@app.get("/createsession")
+async def set_session(request: Request):
+    ssid = request.session.get("ssid")
+    if not ssid:
+        from uuid import uuid4
+        ssid = str(uuid4())
+        request.session["ssid"] = ssid
+        return JSONResponse({"detail": "ssid set sucessfully"}, 200)
+    else:
+        return JSONResponse({"detail": "ssid already exists in the session"}, 400)
 
 if settings.DEBUG == 1:
     print(settings.dict())
--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-import uuid
+from datetime import datetime
 from fastapi import Request
 from app.settings import settings
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
@@ -24,14 +24,34 @@ async def get_route_views(route: str, collection: AsyncIOMotorCollection) -> dic
 
 async def add_view(request: Request, collection: AsyncIOMotorCollection):
     ssid = request.session.get("ssid")
-    # Create a new session if one does not exist
     if not ssid:
-        ssid = str(uuid.uuid4())
-        request.session["ssid"] = ssid
+        print("ssid not found", f"ssid={ssid}") if settings.DEBUG else None
+        return
+
+    # Check if the current route is already present in the database
+    document = await collection.find_one({"ssid": ssid, "routes": request.url.path})
+    if document:
+        return  # Route already exists, nothing to do
+
     # Store the current route in the database or create new entry
-    result = await collection.update_one(
-        {"ssid": ssid},
-        {"$addToSet": {"routes": request.url.path}}
-    )
-    if result.matched_count == 0:
-        await collection.insert_one({"ssid": ssid, "routes": [request.url.path]})
+    update = {
+        "$addToSet": {"routes": request.url.path},
+        "$setOnInsert": {"timestamps.added": datetime.utcnow()},
+    }
+    result = await collection.update_one({"ssid": ssid}, update, upsert=True)
+
+    if result.upserted_id:
+        # The document was inserted, add missing fields
+        await collection.update_one(
+            {"_id": result.upserted_id},
+            {"$currentDate": {"timestamps.last_modified": True}}
+        )
+        print(f"New document created for ssid={ssid}") if settings.DEBUG else None
+    else:
+        # Check if the routes field was updated
+        if result.modified_count > 0:
+            await collection.update_one(
+                {"ssid": ssid},
+                {"$currentDate": {"timestamps.last_modified": True}}
+            )
+            print(f"Document updated for ssid={ssid}") if settings.DEBUG else None
