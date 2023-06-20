e8289efe2dc60c5987f8786297da8e026cf7c8c7
y0nei <xyz.im@proton.me>
2023-06-20 06:47:28 +0200

    feat(frontend): Interactive post item tag pills

--- src/static/postlist.js
+++ src/static/postlist.js
@@ -6,10 +6,18 @@ function changeSort() {
     selectElement.value = selectElement.value === "desc" ? "asc" : "desc";
 }
 
+// Sync text values between post tag pills and tag select select
+// and trigger the submit event from the form to HTMX
+function changeTag() {
+    document.querySelector("#postlist select[name=tag]").value = this.textContent;
+    htmx.trigger("#postlist-header form", "submit");
+    handleTagChange();
+}
+
 // When the tag selection option is set to `All` (or an empty value)
 // the `tag` parameter is excluded from the request made by HTMX
 function handleTagChange() {
     var formElement = document.querySelector("#postlist-header form");
     var tagSelectElement = formElement.querySelector("select[name=tag]")
-    formElement.setAttribute('hx-params', tagSelectElement.value === ""  ? "not tag" : "*");
+    formElement.setAttribute("hx-params", tagSelectElement.value === ""  ? "not tag" : "*");
 }
