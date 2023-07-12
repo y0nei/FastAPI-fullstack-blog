50f08e6ee36165ca0e2c0de6e8339c1d9f5e0b72
y0nei <xyz.im@proton.me>
2023-07-12 13:52:47 +0200

    feat: Improved git version parsing with gitpython

--- src/utils/helpers/version.py
+++ src/utils/helpers/version.py
@@ -1,39 +1,34 @@
-import subprocess
+import time
+from git import Repo
 
-def subprocess_run(args, **kwargs) -> str:
-    kwargs["encoding"] = kwargs.get("encoding", "utf-8")
-    kwargs["stdout"] = subprocess.PIPE
-    kwargs["stderr"] = subprocess.PIPE
-    # raise CalledProcessError if returncode is non-zero
-    kwargs["check"] = True
-    proc = subprocess.run(args, **kwargs)
-    return proc.stdout.strip()
+repo = Repo(search_parent_directories=True)
+sha = repo.head.object.hexsha
 
+if repo.head.is_detached is True:
+    current_branch = repo.head
+else:
+    current_branch = repo.active_branch
 
-# NOTE: After few failed CI pipelines i realized that the rev-parse command
-# fails to get the upstream branch because gitlab runs the ci on a detached
-# head state repo. An ugly workaroud is just to checkout to main for the
-# nescessary CI stage, other than that it works fine, should fix in future.
+def get_git_url_and_branch(repository: Repo = repo) -> dict[str, str]:
+    remote_url = repository.remotes.origin.url
 
-def get_git_url_and_branch() -> dict[str, str]:
-    try:
-        ref = subprocess_run(["git", "rev-parse", "--abbrev-ref", "@{upstream}"])
-    except subprocess.CalledProcessError:
-        ref = subprocess_run(["git", "rev-parse", "--abbrev-ref", "main@{upstream}"])
-    origin, git_branch = ref.split("/", 1)
-    git_url = subprocess_run(["git", "remote", "get-url", origin])
+    if remote_url.startswith("git@"):
+        remote_url = remote_url.replace(":", "/", 2).replace("git@", "https://", 1)
+    if remote_url.endswith(".git"):
+        remote_url = remote_url.replace(".git", "", 1)
 
-    # get https:// url from git@ url
-    if git_url.startswith("git@"):
-        git_url = git_url.replace(":", "/", 2).replace("git@", "https://", 1)
-    if git_url.endswith(".git"):
-        git_url = git_url.replace(".git", "", 1)
+    # Get current active branch or display `detached(commit_sha)`
+    if repo.head.is_detached is True:
+        branch = f"detached({str(repo.head.commit)[:7]})"
+    else:
+        branch = str(repo.active_branch)
 
-    return {"url": git_url, "branch": git_branch}
-
+    return {"url": remote_url, "branch": branch}
 
 def get_git_version() -> str:
-    git_commit_date_hash = subprocess_run(
-        ["git", "show", "-s", "--date=format:%Y.%m.%d", "--format=%cd+%h"]
-    ).replace(".0", ".")
+    commit_date = time.strftime("%Y.%m.%d", time.gmtime(current_branch.commit.committed_date))
+    git_commit_date_hash = f"{commit_date}+{sha[:7]}".replace(".0", ".")
+    if repo.is_dirty():
+        git_commit_date_hash += "+dirty"
+
     return git_commit_date_hash
